c5188f20dc9eac44890fdd14e396bc140a7e1e5d
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-09-30 17:45:26 +0530

    Create alive.py

---
+++ AmeliaRobot/modules/alive.py
@@ -0,0 +1,34 @@
+from AmeliaRobot import dispatcher
+
+from telegram import ParseMode, Update
+from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext.dispatcher import  run_async
+
+GIT_PIC = "https://telegra.ph/file/c5868d73616b42698e8bd.jpg"
+
+GIT_TEXT = """
+Iron Man Is Advanced Group Manager Bot By @Pavakkka
+
+*Contributors/Credits*
+
+> [彡1̸2̸3̸ ᑕᖇEᗩTIOᑎ₴彡](https://t.me/Pavakkka)
+> [☬Ň𝔲𝐛☮𝐤ι𝔫ﻮ☫](https://t.me/AASFCYBERKING)
+> [War-Legend/AmeliaRobot](https://github.com/War-Legend/AmeliaRobot)
+
+[Legend](https://t.me/Pavakkka)
+[Support](https://t.me/PigasusSupport)
+[Updates](https://t.me/PigasusUpdates)
+
+"""
+
+@run_async
+def alive(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(
+        GIT_PIC,
+        caption = GIT_TEXT,
+        parse_mode=ParseMode.MARKDOWN
+        )
+
+ALIVE_HANDLER = CommandHandler("alive", alive)
+
+dispatcher.add_handler(ALIVE_HANDLER)
